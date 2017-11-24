7a882379f382676412caec7e09365353dd77a592
Phil Gates-Shannon <phil.gates-shannon@wmcglobal.com>
2017-11-24 10:25:45 -0500

    Add simple Redux store and a component to display data from the store

--- App.js
+++ App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import Dummy from './Dummy';
+import store from './store';
 
 // TODO Setup Redux store
 // TODO Create skeletons for each of the 5 different views
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your booty.</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <View style={styles.container}>
+                    <Text>Welcome to Flashcards!</Text>
+                    <Dummy />
+                </View>
+            </Provider>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
---
+++ reducer.js
@@ -0,0 +1,7 @@
+// import { combineReducers } from 'redux-immutable';
+
+function basicReducer (initialState={}, action) {
+    return { hello: 'world' };
+}
+
+export default basicReducer;
