8e2dc8344d8fb08920150f09fcd227f24aa6f185
Phil Gates-Shannon <phil.gates-shannon@wmcglobal.com>
2017-11-24 13:24:11 -0500

    Add text input field

--- NewDeck/View.js
+++ NewDeck/View.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, TextInput } from 'react-native';
 import { connect } from 'react-redux';
 
 const styles = StyleSheet.create( {
@@ -12,14 +12,38 @@ const styles = StyleSheet.create( {
     label: {
         fontSize: 30,
         textAlign: 'center'
+    },
+    input: {
+        borderColor: 'gray',
+        borderWidth: 1,
+        fontSize: 24,
+        height: 40,
+        paddingLeft: 5,
+        paddingRight: 5,
+        width: '80%'
     }
 } );
 
 class NewDeckView extends PureComponent {
+    constructor( props ) {
+        super( props );
+        this.handleInputChange = this.handleInputChange.bind( this );
+        this.state = { newDeckName: '' };
+    }
+
+    handleInputChange( text ) {
+        this.setState( { newDeckName: text } );
+    }
+
     render() {
         return (
             <View style={ styles.container }>
                 <Text style={ styles.label }>What is the title of your new deck?</Text>
+                <TextInput
+                    style={ styles.input }
+                    onChangeText={ this.handleInputChange }
+                    value={ this.state.newDeckName }
+                />
             </View>
         );
     }
