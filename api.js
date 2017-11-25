c01e744402205bb7308533391b994151f2a31f06
Phil Gates-Shannon <phil.gates-shannon@wmcglobal.com>
2017-11-25 12:40:54 -0500

    Refactor to functional programming approach to processing data from AsyncStorage

--- api.js
+++ api.js
@@ -3,27 +3,15 @@ import uuidGenerator from 'uuid/v4';
 import { AsyncStorage } from 'react-native';
 import { STORE } from './constants';
 
-export const old_saveNewDeck = function( title ) {
-    // Make sure there's a key for the metadata
-    AsyncStorage.getAllKeys()
-        .then( keys => {
-            if ( !keys.includes( STORE.DECK_METADATA ) ) {
-                // Store an empty object
-                return AsyncStorage.setItem( STORE.DECK_METADATA, JSON.stringify( {} ) );
-            } else {
-                // We can skip this step, so return a fulfilled Promise
-                return Promise.resolve( null );
-            }
-        } )
-        // Get the extant deck metadata
-        .then( () => AsyncStorage.getItem( STORE.DECK_METADATA ) )
-        .then( metadataString => {
-            const metadata = JSON.parse( metadataString );
-        } );
-    // Update the metadata with the new deck
-    // Save the deck metadata
-};
-const logHeader = '{-}';
+import flow from 'lodash/fp/flow';
+import fromPairs from 'lodash/fp/fromPairs';
+import mapValues from 'lodash/fp/mapValues';
+import merge from 'lodash/fp/merge';
+import toPairs from 'lodash/fp/toPairs';
+const fp = { flow, fromPairs, mapValues, merge, toPairs };
+
+const LOG_PREFIX = '{-}';
+
 const emptyAsyncData = _.mapValues( {
     [STORE.DECK_METADATA]: {}
 }, JSON.stringify );
@@ -31,7 +19,6 @@ const emptyAsyncData = _.mapValues( {
 export const saveNewDeck = function( title ) {
     AsyncStorage.getAllKeys()
         .then( keys => {
-            // console.log( `${logHeader} keys :: ${JSON.stringify(keys)}` );
             if ( keys.length > 0 ) {
                 return AsyncStorage.multiGet( keys );
             } else {
@@ -39,34 +26,28 @@ export const saveNewDeck = function( title ) {
             }
         } )
         .then( dataPairs => {
-            // console.log( `${logHeader} dataPairs :: ${JSON.stringify(dataPairs)}` );
             const id = uuidGenerator();
             const payload = { id, title };
             const newData = _.set( {}, [ STORE.DECK_METADATA, id ], payload );
 
-            const temp3 = _.fromPairs( dataPairs );
-            const temp4 = _.mapValues( temp3, JSON.parse );
-
-            const data = _.merge( {}, temp4, newData );
-            // console.log( `${logHeader} data :: ${JSON.stringify(data)}` );
-
-            const temp1 = _.mapValues( data, JSON.stringify );
-            // console.log( `${logHeader} temp1 :: ${JSON.stringify(temp1)}` );
-
-            const temp2 = _.toPairs( temp1 );
-            // console.log( `${logHeader} temp2 :: ${JSON.stringify(temp2)}` );
-
-            return AsyncStorage.multiSet( temp2 );
+            const data = fp.flow(
+                fp.fromPairs,
+                fp.mapValues( JSON.parse ),
+                fp.merge( newData ),
+                fp.mapValues( JSON.stringify ),
+                fp.toPairs
+            )( dataPairs );
+            return AsyncStorage.multiSet( data );
         } )
         .then( errors => {
             if ( errors && errors.length > 0 ) {
                 throw new Error( errors );
             } else {
                 AsyncStorage.getAllKeys()
-                    .then( keys => console.log( `${logHeader} keys :: ${keys}` ) );
+                    .then( keys => console.log( `${LOG_PREFIX} keys :: ${keys}` ) );
             }
         } )
         .catch( error => {
-            console.log( logHeader + error.message );
+            console.log( `${LOG_PREFIX} error message :: ${JSON.stringify(error.message)}` );
         } );
 };
