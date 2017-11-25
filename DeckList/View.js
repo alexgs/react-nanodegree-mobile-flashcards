b52d46f1ec306317aca70b182cdcca2a1ee1f1b8
Phil Gates-Shannon <phil.gates-shannon@wmcglobal.com>
2017-11-25 15:02:23 -0500

    Add placeholder actions and reducer for Deck List

--- DeckList/View.js
+++ DeckList/View.js
@@ -1,13 +1,65 @@
 import React, { PureComponent } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { connect } from 'react-redux';
+import * as actions from './actions';
+
+const styleConstants = {
+    buttonHorizontalPadding: 30,
+    buttonVerticalPadding: 10
+};
+
+const styles = StyleSheet.create( {
+    button: {
+        backgroundColor: 'purple',
+        marginTop: 15,
+        paddingTop: styleConstants.buttonVerticalPadding,
+        paddingBottom: styleConstants.buttonVerticalPadding,
+        paddingLeft: styleConstants.buttonHorizontalPadding,
+        paddingRight: styleConstants.buttonHorizontalPadding
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18
+    },
+    container: {
+        alignItems: 'center',
+        backgroundColor: 'white',
+        flex: 1,
+        justifyContent: 'center'
+    },
+    label: {
+        fontSize: 30,
+        textAlign: 'center'
+    },
+    input: {
+        borderColor: 'gray',
+        borderWidth: 1,
+        fontSize: 24,
+        height: 40,
+        paddingLeft: 5,
+        paddingRight: 5,
+        width: '80%'
+    }
+} );
 
 class DeckListView extends PureComponent {
+    constructor( props ) {
+        super( props );
+        this.handleButtonPress = this.handleButtonPress.bind( this );
+    }
+
+    handleButtonPress() {
+        this.props.dispatch( actions.placeholder() );
+    }
+
     render() {
         return (
             <View>
                 <Text>Hello { this.props.hello }!</Text>
                 <Text>DECK LIST</Text>
+                <TouchableOpacity onPress={ this.handleButtonPress } style={ styles.button }>
+                    <Text style={ styles.buttonText }>DO PLACEHOLDER</Text>
+                </TouchableOpacity>
             </View>
         );
     }
--- reducer.js
+++ reducer.js
@@ -1,13 +1,15 @@
 import Immutable from 'immutable';
 import { combineReducers } from 'redux-immutable';
+import deckListReducer from './DeckList/reducer';
 import newDeckReducer from './NewDeck/reducer';
 import { STORE } from './constants';
 
-function basicReducer (state=Immutable.Map(), action) {
+function basicReducer( state = Immutable.Map(), action ) {
     return Immutable.fromJS( { hello: 'Susannah' } );
 }
 
 export default combineReducers( {
-    [STORE.DECKS]: newDeckReducer,
+    [STORE.DECK_METADATA]: newDeckReducer,
+    [STORE.DECKS]: deckListReducer,
     foo: basicReducer
 } );
