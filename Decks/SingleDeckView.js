b2bcd24f9ba4fc25fe5748d3c6569b6318d95c2d
Phil Gates-Shannon <phil.gates-shannon@wmcglobal.com>
2017-11-27 08:40:15 -0500

    Implement retrieving deck metadata & data from store and basic display of same

--- Decks/SingleDeckView.js
+++ Decks/SingleDeckView.js
@@ -1,18 +1,36 @@
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { connect } from 'react-redux';
+import { STORE } from '../constants';
+
+// TODO Add buttons
+// TODO Layout and styles
 
 class SingleDeckView extends PureComponent {
     static propTypes = {};
 
     render() {
+        const deckId = this.props.navigation.state.params.id;
+        const deckMetadata = this.props[ STORE.DECK_METADATA ].get( deckId );
+        const deckData = this.props[ STORE.DECKS ].get( deckId );
+        const cardCount = deckData ? deckData.size() : 0;
+        const cardCountLabel = cardCount === 1 ? 'card' : 'cards';
+
         return (
             <View>
-                <Text>Single Deck View</Text>
-                <Text>Deck ID: { this.props.navigation.state.params.id }</Text>
+                <Text>{ deckMetadata.get( 'title' ) }</Text>
+                <Text>{ cardCount } { cardCountLabel }</Text>
             </View>
         );
     }
 }
 
-export default SingleDeckView;
+function mapStateToProps( state ) {
+    return {
+        [STORE.DECK_METADATA]: state.get( STORE.DECK_METADATA ),
+        [STORE.DECKS]: state.get( STORE.DECKS )
+    };
+}
+
+export default connect( mapStateToProps )( SingleDeckView );
